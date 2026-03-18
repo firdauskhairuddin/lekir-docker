ea3af6d8f3d30f075e3e78f78ac37f93f8daaf24
firdauskhairuddin <163268463+firdauskhairuddin@users.noreply.github.com>
2026-03-18 14:05:18 +0800

    Create stop-lekir.sh

---
+++ stop-lekir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker compose down
+
+echo ""
+echo "--------------------------------------"
+echo "✅ Lekir Framework Stopped Successfully"
+echo "All containers are now stopped."
+echo "You can restart the lab anytime with ./start-lekir.sh"
+echo "--------------------------------------"
