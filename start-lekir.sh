9a1160f0f0385b4fc0569f8c7110544645d35298
firdauskhairuddin <163268463+firdauskhairuddin@users.noreply.github.com>
2026-03-18 14:03:51 +0800

    Create start-lekir.sh

---
+++ start-lekir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Start Lekir containers
+docker compose up -d
+
+echo -e "\n🚀 Lekir Framework is ready!"
+echo "🌐 URL: http://localhost:1337"
+echo "👤 Username: admin"
+echo "🔑 Password: password"
